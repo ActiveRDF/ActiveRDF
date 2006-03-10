4273005bc1b7498659f1e1f955001b7976a830ad
Renaud Delbru <renaud.delbru@deri.org>
2006-03-10 20:06:29 +0000

    Start of IdentifiedResource class

--- src/identified_resource.rb
+++ src/identified_resource.rb
@@ -1,40 +1,55 @@
-# Class IdentifiedResource
+# = identified_resource.rb
+#
+# Class definition of an identified resource.
+# This resource is the superclass of all rdf resource identified and contains
+# all instance methods and class methods to manipulate related attributes, search
+# other resources related, etc...
+#
+# == Project
+#
+# * ActiveRDF
+# <http://m3pe.org/activerdf/>
+#
+# == Authors
 # 
-class IdentifiedResource < Resource
-include BasicIdentifiedResource
-include AttributeContainer
-include InstanciatedResourceMethod
-include DynamicQueryMethod
+# * Eyal Oren <first dot last at deri dot org>
+# * Renaud Delbru <first dot last at deri dot org>
+#
+# == Copyright
+#
+# (c) 2005-2006 by Eyal Oren and Renaud Delbru - All Rights Reserved
+#
+# == To-do
+#
+# * To-do 1
+#
 
-# Public Items
+require 'basic_identified_resource'
+require 'module/attributes_container'
+require 'module/instanciated_resource_method'
 
-		# 
+class IdentifiedResource < BasicIdentifiedResource
 
-		# Constructors
+	include AttributesContainer
+	include InstanciatedResourceMethod
+	
+	# if no subclass is specified, this is an rdfs:resource
+	@@_class_uri[self] = 'http://www.w3.org/2000/01/rdf-schema#Resource'
 
-		# Accessor Methods
+	def initialize(uri, attributes = nil)
+		super(uri)
+		
+		@_attributes = Hash.new
+		self.class.predicates.each_key do |attr_name|
+			@_attributes[attr_name] = nil
+		end
+		
+		update_attributes(attributes) if !attributes.nil?
+	end
 
-		# Operations
-
-# Protected Items
-
-		# 
-
-		# Constructors
-
-		# Accessor Methods
-
-		# Operations
-
-# Private Items
-
-		# 
-
-		# Constructors
-
-		# Accessor Methods
-
-		# Operations
+#----------------------------------------------#
+#               PUBLIC METHODS                 #
+#----------------------------------------------#
 
 end
 
