204d7d81fe154904daef870ec6ef8f4058889b99
Renaud Delbru <renaud.delbru@deri.org>
2006-03-15 14:54:45 +0000

    Removed BasicIdentifiedResource. Removed to_identified_resource method and added uri variable and create method.

--- src/identified_resource.rb
+++ src/identified_resource.rb
@@ -24,37 +24,50 @@
 # * To-do 1
 #
 
-require 'basic_identified_resource'
 require 'module/attributes_container'
 require 'module/instanciated_resource_method'
+require 'module/dynamic_query_method'
 
-class IdentifiedResource < BasicIdentifiedResource
+class IdentifiedResource < Resource
 
 	include AttributesContainer
 	include InstanciatedResourceMethod
+	extend DynamicQueryMethod
+	
+	# IdentifiedResource isn't an abstract class like Resource, we can instantiate it.
+	public_class_method :new
 	
 	# if no subclass is specified, this is an rdfs:resource
 	@@_class_uri[self] = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Resource'
 
-	def initialize(uri, attributes = nil)
-		super(uri)
-		
-		@_attributes = Hash.new
-		self.class.predicates.each_key do |attr_name|
-			@_attributes[attr_name] = nil
+	# URI of the resource
+	attr_reader :uri
+
+	def initialize(uri)
+		if uri.nil? or uri.empty?
+			raise(ActiveRdfError, 'Resource URI is invalid. Cannot instanciated the object.')
 		end
-		
-		update_attributes(attributes) if !attributes.nil?
+		@uri = uri
 	end
 
 #----------------------------------------------#
 #               PUBLIC METHODS                 #
 #----------------------------------------------#
-
-	public
 	
-	def to_identified_resource
-		raise(NoMethodError, "This object is already an identified resource.")
+	public
+
+  # Create a new identified resource if it doesn't exists, otherwise laod the resource.
+  # This method must be called instead of the original new method, because
+  # it calls the creation method of the NodeFactory which allows to keep only
+  # one instance of the resource in memory.
+  #
+  # Arguments:
+  # * +uri+ [<tt>String</tt>]: Uri of the resource.
+  #
+  # Return:
+  # * [<tt>IdentifiedResource</tt>] The new identified resource
+	def self.create(uri)
+		return NodeFactory.create_identified_resource(uri, self)
 	end
 
 end
