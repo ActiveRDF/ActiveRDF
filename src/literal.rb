9d342bb3025d338762a1388953e9d8fcbce06358
Renaud Delbru <renaud.delbru@deri.org>
2006-03-15 14:55:24 +0000

    Added create method and determine_type method

--- src/literal.rb
+++ src/literal.rb
@@ -36,5 +36,41 @@ class Literal; implements Node
 		self.type = type
 	end
 	
+#----------------------------------------------#
+#               PUBLIC METHODS                 #
+#----------------------------------------------#
+	
+	public
+	
+  # Create a new Literal.
+  # Determine the type of the value.
+  #
+  # Arguments:
+  # * +value+: Value of the Literal
+  #
+  # Return:
+  # * [<tt>Literal</tt>] The new Literal node
+	def self.create(value)
+		type = determine_type(value)
+		return NodeFactory.create_literal(value.to_s, type)
+	end
+	
+#----------------------------------------------#
+#               PRIVATE METHODS                #
+#----------------------------------------------#
+
+ 	private
+  
+  # Determine the value type of the literal.
+  #
+  # Arguments:
+  # * +value+: Value of the Literal
+  #
+  # Return:
+  # * [<tt>String</tt>] Value type
+	def self.determine_type(value)
+		return 'Literal type is not yet implemented.'
+	end
+	
 end
 
