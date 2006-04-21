7b3998e7ab00088da4adddcb9635326c18b6510a
Eyal Oren <eyal.oren@deri.org>
2006-04-21 18:14:28 +0100

    added add_predicate to IdentifiedResource (only on class-level)

--- active_rdf/src/core/identified_resource.rb
+++ active_rdf/src/core/identified_resource.rb
@@ -74,5 +74,71 @@ class IdentifiedResource < Resource
 		return uri
 	end
 
+	def ==(b)
+		return eql?(b)
+	end
+
+	def eql?(b)
+		return b.uri == uri
+	end
+
+	def hash
+		uri.hash
+	end
+
+	# Adds the predicate to the class level of the resource
+	def self.add_predicate uri, localname=nil
+		$logger.debug "adding predicate #{uri} to class #{self}"
+
+		# if we received a Resource as URI then that is ok, otherwise if we resource 
+		# a String we create an IdentifiedResource ourselves, otherwise we throw an 
+		# error
+		unless uri.respond_to? :uri
+			if uri.kind_of? String
+				uri = IdentifiedResource.create uri
+			else 
+				raise ActiveRdfError, "add_predicate should be given IdentifiedResource or String, received a #{uri.class}"
+			end
+		end
+
+		# if not given a localname we construct one ourselves
+		localname = uri.local_part if localname.nil?
+
+		# getting the predicates hash of this class
+		# (hashing localname -> IdentifiedResource-fullURI
+		class_hash = @@predicates[self]
+
+
+		# initialise predicates hash for this class if undefined
+		@@predicates[self] = (class_hash = Hash.new) if class_hash.nil?
+
+
+		$logger.debug "adding predicate #{uri}; class hash has size #{class_hash.size}"
+
+		# if localname collision detected
+		if class_hash.include? localname
+			$logger.debug "localname collision when adding predicate #{uri}"
+
+			# if this predicate was already defined for the class, we don't do 
+			# anything and just return the predicate
+			if class_hash[localname] == uri
+				return uri
+			else
+				#otherwise we raise a collision error
+			 	raise ActiveRdfError, "local_name collision, given #{uri} but we already have a predicate called #{localname} defined with URI #{class_hash[localname]}"
+			end
+		end
+		
+		# add the localname with the given URI to the predicates hash of this class
+		class_hash[localname] = uri
+
+		# TODO: add schema information to database: 
+		# uri rdf:type property; uri rdfs:domain self
+		
+		$logger.debug "added predicate; class hash has size #{class_hash.size}"
+		$logger.debug "@@predicates: #{@@predicates}"
+		return uri
+	end
+	
 end
 
