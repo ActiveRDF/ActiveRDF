bf933cd9b0267c58c75bd5700ea5f45277a4177e
Eyal Oren <eyal.oren@deri.org>
2006-04-28 12:38:09 +0100

    updates YARS testsuite to include all tests

--- active_rdf/test/ts_yars.rb
+++ active_rdf/test/ts_yars.rb
@@ -25,20 +25,22 @@ require 'test/unit/ui/console/testrunner'
 
 # Constant definition
 
-DB_HOST = 'm3pe.org'
+DB_HOST = 'opteron'
 DB = :yars
 
 # Unit test include
 
 # NodeFactory Tests
+require 'test/node_factory/test_connection'
 require 'test/node_factory/test_initialisation_connection'
 require 'test/node_factory/test_create_literal'
 require 'test/node_factory/test_create_basic_resource'
 require 'test/node_factory/test_create_identified_resource_with_unknown_type'
 require 'test/node_factory/test_create_identified_resource_on_person_type'
 require 'test/node_factory/test_person_methods'
+require 'test/node_factory/test_equals'
 
-# Yars adpater Tests
+# Yars adapter Tests
 require 'test/adapter/yars/test_yars_adapter'
 require 'test/adapter/yars/test_yars_adapter_add'
 require 'test/adapter/yars/test_yars_adapter_remove'
@@ -46,56 +48,67 @@ require 'test/adapter/yars/test_yars_basic_query'
 require 'test/adapter/yars/test_yars_joint_query'
 
 # Core Tests
-require 'test/resource/test_resource'
-require 'test/resource/test_resource_get'
-require 'test/resource/test_resource_find'
-require 'test/resource/test_identified_resource'
-require 'test/resource/test_identifiedresource_get'
-require 'test/resource/test_identifiedresource_find'
-require 'test/resource/test_identifiedresource_create'
-require 'test/resource/test_identifiedresource_attributescontainer'
+require 'test/core/resource/test_resource'
+require 'test/core/resource/test_resource_get'
+require 'test/core/resource/test_resource_find'
+require 'test/core/resource/test_identified_resource'
+require 'test/core/resource/test_identifiedresource_get'
+require 'test/core/resource/test_identifiedresource_find'
+require 'test/core/resource/test_identifiedresource_create'
+require 'test/core/resource/test_identifiedresource_attributescontainer'
 
 # NamespaceFactory Test
 require 'test/namespace_factory/test_namespace_factory'
 
 class TestSuite_AllTests
-    def self.suite
-        suite = Test::Unit::TestSuite.new("ActiveRDF Tests")
-        
-        # NodeFactory Tests
-        suite << TestNodeFactoryInitialisationConnection.suite
-        suite << TestNodeFactoryLiteral.suite
-        suite << TestNodeFactoryBasicResource.suite
-        
-        # Yars Adapter Tests
-        suite << TestYarsAdapter.suite
-        suite << TestYarsAdapterAdd.suite
-        suite << TestYarsAdapterRemove.suite
-        suite << TestYarsAdapterBasicQuery.suite
-        suite << TestYarsAdapterJointQuery.suite
-        
-        # Resource Tests
-        suite << TestResource.suite
-        suite << TestResourceGet.suite
-        suite << TestResourceFind.suite
-        suite << TestIdentifiedResource.suite
-        suite << TestIdentifiedResourceGet.suite
-        suite << TestIdentifiedResourceFind.suite
-        
-        # NodeFactory Tests suite
-        suite << TestNodeFactoryUnknownIdentifiedResource.suite
-        suite << TestNodeFactoryIdentifiedResource.suite
-        suite << TestNodeFactoryPerson.suite
-        
-        # Resource Tests suite
-        suite << TestIdentifiedResourceCreate.suite
-        suite << TestAttributesContainer.suite
-        
-        # NamespaceFactory Tests
-        suite << TestNamespaceFactory.suite    
-               
-        return suite
-    end
+	def self.test_adapter suite
+		# Yars Adapter Tests
+		suite << TestYarsAdapter.suite
+		suite << TestYarsAdapterAdd.suite
+		suite << TestYarsAdapterRemove.suite
+		suite << TestYarsAdapterBasicQuery.suite
+		suite << TestYarsAdapterJointQuery.suite
+	end
+
+	def self.test_resource suite
+		# Resource Tests
+		suite << TestResource.suite
+		suite << TestResourceGet.suite
+		suite << TestResourceFind.suite
+		suite << TestIdentifiedResource.suite
+		suite << TestIdentifiedResourceGet.suite
+		suite << TestIdentifiedResourceFind.suite
+		suite << TestIdentifiedResourceCreate.suite
+		suite << TestAttributesContainer.suite
+	end
+		
+	def self.test_namespace suite
+		# NamespaceFactory Tests
+		suite << TestNamespaceFactory.suite    
+	end
+
+	def self.test_nodefactory suite
+		# NodeFactory Tests
+		#suite << TestNodeFactoryInitialisationConnection.suite
+		#suite << TestNodeFactoryLiteral.suite
+		#suite << TestNodeFactoryBasicResource.suite
+		#suite << TestResourceEquality.suite
+		#suite << TestNodeFactoryUnknownIdentifiedResource.suite
+		#suite << TestNodeFactoryIdentifiedResource.suite
+		#suite << TestNodeFactoryPerson.suite
+		suite << TestConnection.suite
+	end
+
+	def self.suite
+		suite = Test::Unit::TestSuite.new("ActiveRDF Tests")
+		
+		#test_nodefactory suite
+		#test_adapter suite
+		#test_resource suite
+		#test_namespace suite
+					 
+		return suite
+	end
 end
 
 Test::Unit::UI::Console::TestRunner.run(TestSuite_AllTests)
