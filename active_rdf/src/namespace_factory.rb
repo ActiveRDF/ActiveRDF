18af63d856df2485ffc7f0ef03f2a011f0695da0
Eyal Oren <eyal.oren@deri.org>
2006-05-12 08:19:00 +0100

    changed namespace factory to return arbitrary resources, using prefix

--- active_rdf/src/namespace_factory.rb
+++ active_rdf/src/namespace_factory.rb
@@ -48,34 +48,38 @@ class NamespaceFactory
 		@@_namespaces[prefix] = uri
 	end
 	
-	# Get a namespace
-	#
-	# Arguments:
-	# * +prefix+ [<tt>Symbol</tt>]: Prefix of the namespace
-	#
-	# Return:
-	# * [<tt>IdentifiedResource</tt>] Resource wrapping the namespace
-	def self.get(prefix)
-		if !prefix.is_a?(Symbol)
-			raise(NamespaceFactoryError, "In #{__FILE__}:#{__LINE__}, prefix is not a Symbol, received #{prefix.class}.")
-		end
+	#def self.get(prefix)
+	#	case prefix
+	#	when :rdf_type
+	#		get(:rdf, 'type')
+	#	else
+	#		raise NamespaceFactoryError, 'get refactored: use get(:prefix, localpart)'
+	#	end
+	#end
 
-		if !namespaces.key?(prefix)
-			raise(NamespaceFactoryError, "In #{__FILE__}:#{__LINE__}, unknown namespace.")
-		end
-		
-		return NodeFactory.create_basic_resource(namespaces[prefix])
+	# Returns namespace prefix concatenated with local name
+	# Arguments: prefix (symbol), localname
+	# Returns: IdentifiedResource
+	def self.get prefix, localname
+		raise NamespaceFactoryError, 'prefix #{prefix.class} must be a symbol' unless prefix.is_a?(Symbol)
+		raise NamespaceFactoryError, 'unknown namespace prefix #{prefix}' unless namespaces.key?(prefix)
+		return NodeFactory.create_basic_resource(namespaces[prefix] + localname.to_s)
 	end
+
 
 	# Load the commun uri in the factory.
 	def self.load_namespaces
-		add(:rdf_type, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-		add(:rdfs_domain, 'http://www.w3.org/2000/01/rdf-schema#domain')
-		add(:rdfs_range, 'http://www.w3.org/2000/01/rdf-schema#range')
-		add(:rdfs_subclass, 'http://www.w3.org/2000/01/rdf-schema#subClassOf')
-		add(:owl_thing, 'http://www.w3.org/2002/07/owl#Thing')
-		add(:rdfs_label, 'http://www.w3.org/2000/01/rdf-schema#label')
-		add(:rdfs_comment, 'http://www.w3.org/2000/01/rdf-schema#comment')
+		add(:rdf,'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+		add(:rdfs,'http://www.w3.org/2000/01/rdf-schema#')
+		add(:owl, 'http://www.w3.org/2002/07/owl#')
+
+		#add(:rdf_type, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+		#add(:rdfs_domain, 'http://www.w3.org/2000/01/rdf-schema#domain')
+		#add(:rdfs_range, 'http://www.w3.org/2000/01/rdf-schema#range')
+		#add(:rdfs_subclass, 'http://www.w3.org/2000/01/rdf-schema#subClassOf')
+		#add(:owl_thing, 'http://www.w3.org/2002/07/owl#Thing')
+		#add(:rdfs_label, 'http://www.w3.org/2000/01/rdf-schema#label')
+		#add(:rdfs_comment, 'http://www.w3.org/2000/01/rdf-schema#comment')
 	end
 
 #----------------------------------------------#
