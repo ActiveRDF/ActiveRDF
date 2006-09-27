717e752cee93bf8978c91b146d7147c3313f199f
Eyal Oren <eyal.oren@deri.org>
2006-09-27 11:13:37 +0100

    deleted old activerdf tree as preparation for merging the new implementation

--- active_rdf/src/core/standard_classes.rb
+++
@@ -1,9 +0,0 @@
-require 'active_rdf'
-class RdfsClass < IdentifiedResource
-	set_class_uri 'http://www.w3.org/2000/01/rdf-schema#Class'
-end
-
-class RdfProperty < IdentifiedResource
-	set_class_uri 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property'
-	add_predicate IdentifiedResource.new('http://www.w3.org/2000/01/rdf-schema#domain')
-end
