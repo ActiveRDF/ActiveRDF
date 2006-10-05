12de440ff20a7d330a358bd833c06aee56909fea
Eyal Oren <eyal.oren@deri.org>
2006-10-05 19:16:41 +0100

    fixed small bug in jars2 query generation (forgot space before dot on each line)

--- src/queryengine/query2jars2.rb
+++ src/queryengine/query2jars2.rb
@@ -6,7 +6,7 @@ class Query2Jars2
 		if query.select?
 			# concatenate each where clause using space: s p o
 			# and then concatenate the clauses using dot: s p o . s2 p2 o2 .
-			str << "#{query.where_clauses.collect{|w| w.join(' ')}.join(".\n")} ."
+			str << "#{query.where_clauses.collect{|w| w.join(' ')}.join(" .\n")} ."
 		end
 		str
 	end
