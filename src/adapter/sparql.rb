da40d3879116c8f27ccb783f05cb17158d75540b
Sebastian Gerke <sebastian.gerke@deri.org>
2006-10-05 19:19:14 +0100

    Improved (faster) XML parsing of SPARQL results by using XML stream parsing.

--- src/adapter/sparql.rb
+++ src/adapter/sparql.rb
@@ -7,41 +7,42 @@ require 'cgi'
 require 'active_rdf'
 
 class SparqlAdapter
-  ConnectionPool.register_adapter(:sparql, self)
+	ConnectionPool.register_adapter(:sparql, self)
 
-  def reads?
-    true
-  end
+	def reads?
+		true
+	end
+	
+	def writes?
+		false
+	end
+	
+	# Instantiate the connection with the SPARQL Endpoint.
+	def initialize(params = {})	
+		@host = params[:host] || 'm3pe.org'
+		@path = params[:path] || 'repositories/'
+		@port = params[:port] || 8080
+		@context = params[:context] || 'test-people'
+		@result_format = params[:result_format] || :sparql_xml
+		
+		known_formats = [:xml, :json, :sparql_xml]
+		raise ActiveRdfError, "Result format unsupported" unless known_formats.include?(@result_format)
+		
+		# We don't open the connection yet but let each HTTP method open and close 
+		# it individually. It would be more efficient to pipeline methods, and keep 
+		# the connection open continuously, but then we would need to close it 
+		# manually at some point in time, which I do not want to do.
+		
+		@sparql = Net::HTTP.new(@host,@port)
+	end
 
-  def writes?
-    false
-  end
-
-  # Instantiate the connection with the SPARQL Endpoint.
-  def initialize(params = {})
-    @host = params[:host] || 'm3pe.org'
-    @path = params[:path] || 'repositories/'
-    @port = params[:port] || 8080
-    @context = params[:context] || 'test-people'
-    @result_format = params[:result_format] || :sparql_xml
-
-    known_formats = [:xml, :json, :sparql_xml]
-    raise ActiveRdfError, "Result format unsupported" unless known_formats.include?(@result_format)
-
-    # We don't open the connection yet but let each HTTP method open and close
-    # it individually. It would be more efficient to pipeline methods, and keep
-    # the connection open continuously, but then we would need to close it
-    # manually at some point in time, which I do not want to do.
-
-    @sparql = Net::HTTP.new(@host,@port)
-  end
-
-  # query datastore with query string (SPARQL), returns array with query results
-  def query(query)
+	# query datastore with query string (SPARQL), returns array with query results
+	def query(query)
     qs = Query2SPARQL.translate(query)
 		execute_sparql_query(qs, header(query)) #, query.select_clauses.size)
 	end
-
+	
+	
 	def execute_sparql_query(qs, header) #,nr_of_clauses)
 		# TODO: can we really remove this (see line 66)
     #clauses = query.select_clauses.size
@@ -59,7 +60,9 @@ class SparqlAdapter
     when :json
       parse_sparql_query_result_json response.body
     when :xml, :sparql_xml
-      parse_sparql_query_result_xml response.body
+      parse_sparql_query_result_xml_stream response.body
+     	# parse_sparql_query_result_xml response.body
+     	# can be removed if everything is working fine. (line 129ff can be removed then as well)
     end
 
     if block_given?
@@ -69,30 +72,30 @@ class SparqlAdapter
     else
       results
     end
-  end
+	end
+	
+	private
 
-  private
-
-  # constructs correct HTTP header for selected query-result format
-  def header(query)
-    case @result_format
-    when :json
-      header = { 'accept' => 'application/sparql-results+json' }
-    when :xml
-      header = { 'accept' => 'application/rdf+xml' }
-    when :sparql_xml
-      header = { 'accept' => 'application/sparql-results+xml' }
-    end
-  end
+	# constructs correct HTTP header for selected query-result format
+	def header(query)
+		case @result_format
+		when :json
+			header = { 'accept' => 'application/sparql-results+json' }
+		when :xml
+			header = { 'accept' => 'application/rdf+xml' }
+		when :sparql_xml
+		  header = { 'accept' => 'application/sparql-results+xml' }
+		end
+	end
 
   # parse json query results into array
-  def parse_sparql_query_result_json(query_result)
+	def parse_sparql_query_result_json(query_result)
     require 'json'
-
+    
     parsed_object = JSON.parse(query_result)
     return [] if parsed_object.nil?
-
-    results = []
+    
+    results = []    
     vars = parsed_object['head']['vars']
     objects = parsed_object['results']['bindings']
     if vars.length > 1
@@ -104,7 +107,7 @@ class SparqlAdapter
         results << result
       end
     else
-      objects.each do |obj|
+      objects.each do |obj| 
         obj.each_value do |e|
           results << create_node(e['type'], e['value'])
         end
@@ -112,7 +115,16 @@ class SparqlAdapter
     end
     return results
   end
-
+  
+  
+  def parse_sparql_query_result_xml_stream(qr)
+    require 'rexml/document'
+    require 'adapter/sparql_result_parser'
+    parser = SparqlResultParser.new
+    REXML::Document.parse_stream(qr, parser)
+    parser.result
+  end
+  
   # parse xml query results into array
   def parse_sparql_query_result_xml(query_result)
     require 'rexml/document'
@@ -129,24 +141,24 @@ class SparqlAdapter
           result.each_element_with_attribute('name', v) do |binding|
             binding.elements.each do |e|
               myResult << create_node(e.name, e.text)
-            end
-          end
+            end            
+          end          
         end
         results << myResult
       end
-
+      
     else
-      objects.each do |bs|
+      objects.each do |bs| 
         bs.elements.each("binding") do |b|
           b.elements.each do |e|
-            results << create_node(e.name, e.text)
-          end
+						results << create_node(e.name, e.text)
+					end
         end
       end
     end
     return results
   end
-
+  
   # create ruby objects for each RDF node
   def create_node(type, value)
     case type
