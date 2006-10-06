eff0bfe622fe7aead0dacda7723d927647e523c7
Eyal Oren <eyal.oren@deri.org>
2006-10-06 11:44:06 +0100

    adding query2jars2 translator (used in jars2 adapter)

--- src/queryengine/query2jars2.rb
+++ src/queryengine/query2jars2.rb
@@ -7,6 +7,11 @@ class Query2Jars2
 			# concatenate each where clause using space: s p o
 			# and then concatenate the clauses using dot: s p o . s2 p2 o2 .
 			str << "#{query.where_clauses.collect{|w| w.join(' ')}.join(" .\n")} ."
+			# TODO: should we maybe reverse the order on the where_clauses? it depends 
+			# on Andreas' answer of the best order to give to jars2. Users would 
+			# probably put the most specific stuff first, and join to get the 
+			# interesting information. Maybe we should not touch it and let the user 
+			# figure it out.
 		end
 		str
 	end
