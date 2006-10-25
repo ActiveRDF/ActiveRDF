5d34ac4dfd413a1fad1058f6f125c05e33bf69c9
Benjamin Heitmann <benjamin.heitmann@deri.org>
2006-10-25 16:44:57 +0100

    fixed the relative loading of the adapters

--- lib/active_rdf.rb
+++ lib/active_rdf.rb
@@ -6,7 +6,9 @@
 
 # adding active_rdf subdirectory to the ruby loadpath
 file = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-$: << File.dirname(File.expand_path(file)) + '/active_rdf/'
+this_dir = File.dirname(File.expand_path(file))
+$: << this_dir
+$: << this_dir + '/active_rdf/'
 
 require 'active_rdf_helpers'
 require 'active_rdf_log'
@@ -19,17 +21,27 @@ require 'objectmanager/namespace'
 require 'federation/connection_pool'
 require 'queryengine/query'
 
+
+def load_adapter s
+  begin
+    require s
+  rescue StandardError
+  end
+end
+
+
 #determine if we are installed as a gem right now:
 if Gem::cache().search("activerdf").empty?
   #we are not running as a gem
   $log.info 'ActiveRDF is NOT installed as a Gem'
-  require '../../activerdf-rdflite/lib/activerdf_rdflite/rdflite'
-  require '../../activerdf-redland/lib/activerdf_redland/redland'
-  require '../../activerdf-sparql/lib/activerdf_sparql/sparql'
-  require '../../activerdf-yars/lib/activerdf_yars/yars'
+  load_adapter this_dir + '/../activerdf-rdflite/lib/activerdf_rdflite/rdflite'
+  load_adapter this_dir + '/../activerdf-redland/lib/activerdf_redland/redland'
+  load_adapter this_dir + '/../activerdf-sparql/lib/activerdf_sparql/sparql'
+  load_adapter this_dir + '/../activerdf-yars/lib/activerdf_yars/jars2'
 else
   #we are indeed running as a gem
 	require 'gem_plugin'
   $log.info 'ActiveRDF is installed as a Gem'
   GemPlugin::Manager.instance.load "activerdf" => GemPlugin::INCLUDE
 end 
+  
