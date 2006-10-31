d55b527a58af12abdefe4767bf635f4f24fe557a
Benjamin Heitmann <benjamin.heitmann@deri.org>
2006-10-31 14:00:35 +0000

    added get_read_only_adapter and get_different_adapter and get_different_write_adapter to common.rb

--- test/common.rb
+++ test/common.rb
@@ -15,6 +15,34 @@ def get_adapter
 	end
 end
 
+def get_read_only_adapter
+	types = ConnectionPool.adapter_types
+  if types.include?(:sparql)
+		ConnectionPool.add :type => :sparql
+	else
+		raise ActiveRdfError, "no suitable read only adapter found for test"
+	end
+	
+end
+
+# TODO make this work with a list of existing adapters, not only one
+def get_different_adapter(existing_adapter)
+	types = ConnectionPool.adapter_types
+	if types.include?(:rdflite) and existing_adapter.class != RDFLite
+		ConnectionPool.add :type => :rdflite
+	elsif types.include?(:redland) and existing_adapter.class != RedlandAdapter
+		ConnectionPool.add :type => :redland
+	elsif types.include?(:sparql) and existing_adapter.class != SparqlAdapter
+		ConnectionPool.add :type => :sparql
+	elsif types.include?(:yars) and existing_adapter.class != YarsAdapter
+		ConnectionPool.add :type => :yars
+	elsif types.include?(:jars2) and existing_adapter.class != Jars2Adapter
+		ConnectionPool.add :type => :jars2
+	else
+		raise ActiveRdfError, "only one adapter on this system, or no suitable adapter found for test"
+	end
+end
+
 def get_all_read_adapters
 	types = ConnectionPool.adapter_types
 	adapters = types.collect {|type| ConnectionPool.add :type => type }
@@ -39,5 +67,21 @@ def get_write_adapter
 		ConnectionPool.add :type => :jars2
 	else
 		raise ActiveRdfError, "no suitable adapter found for test"
+	end
+end
+
+# TODO use a list of exisiting adapters not only one
+def get_different_write_adapter(existing_adapter)
+	types = ConnectionPool.adapter_types
+	if types.include?(:rdflite) and existing_adapter.class != RDFLite
+		ConnectionPool.add :type => :rdflite
+	elsif types.include?(:redland) and existing_adapter.class != RedlandAdapter
+		ConnectionPool.add :type => :redland
+	elsif types.include?(:yars) and existing_adapter.class != YarsAdapter
+		ConnectionPool.add :type => :yars
+	elsif types.include?(:jars2) and existing_adapter.class != Jars2Adapter
+		ConnectionPool.add :type => :jars2
+	else
+		raise ActiveRdfError, "only one write adapter on this system, or no suitable write adapter found for test"
 	end
 end
