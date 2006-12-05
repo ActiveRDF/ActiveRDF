6970718038ea23f8f7a0717a6efb0ada840d5330
Eyal Oren <eyal.oren@deri.org>
2006-12-05 12:48:25 +0000

    fetching adapter strips fragment identifier before fetching, so that rdf:about="" works properly

--- activerdf-rdflite/lib/activerdf_rdflite/fetching.rb
+++ activerdf-rdflite/lib/activerdf_rdflite/fetching.rb
@@ -1,7 +1,28 @@
+#class String
+#  alias _match match
+#  def match(*args)
+#    m = _match(args.first)
+#    if m && m.length > 1
+#      args[1..-1].each_with_index do |name, index|
+#        m.instance_eval "def #{name}; self[#{index+1}] end"
+#      end
+#    end
+#    m
+#  end
+#end
+
 class FetchingAdapter < RDFLite
   ConnectionPool.register_adapter(:fetching,self)
 
+	# fetches RDF/XML data from given url and adds it to the datastore, using the 
+	# source url as context identifier.
   def fetch url
+		return unless url.match(/http:\/\/(.*)/)
+
+		# remove fragment identifier from url
+		hashidx = url.rindex('#')
+		url = url[0..url.rindex('#')-1] unless hashidx.nil?
+
 		$activerdflog.debug "fetching from #{url}"
 
 		#TODO: waiting for an answer on how to enable --scan over ruby api
@@ -20,4 +41,3 @@ class FetchingAdapter < RDFLite
 		add_ntriples(triples, context)
   end
 end
-
