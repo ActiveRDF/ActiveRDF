a3bd144dca32e9c9ba7e9e34b449b0d514dad128
Eyal Oren <eyal.oren@deri.org>
2007-02-21 12:30:54 +0000

    fixing parsing of literals with encoded HTML data

--- lib/active_rdf/queryengine/ntriples_parser.rb
+++ lib/active_rdf/queryengine/ntriples_parser.rb
@@ -3,38 +3,48 @@
 # License:: LGPL
 require 'active_rdf'
 require 'uuidtools'
+require 'strscan'
 
 # ntriples parser
 class NTriplesParser
   # parses an input string of ntriples and returns a nested array of [s, p, o] 
   # (which are in turn ActiveRDF objects)
   def self.parse(input)
-
 		# need unique identifier for this batch of triples (to detect occurence of 
 		# same bnodes _:#1
 		uuid = UUID.random_create.to_s
 
     input.collect do |triple|
-			nodes = triple.scan(Node)
+      nodes = []
+      scanner = StringScanner.new(triple)
+      scanner.skip(/\s+/)
+      while not scanner.eos?
+        nodes << scanner.scan(Node)
+        scanner.skip(/\s+/)
+        scanner.terminate if nodes.size == 3 
+      end
 
 			# handle bnodes if necessary (bnodes need to have uri generated)
 			subject = case nodes[0]
 								when BNode
 									RDFS::Resource.new("http://www.activerdf.org/bnode/#{uuid}/#$1")
-								else
-									RDFS::Resource.new(nodes[0])
+                when Resource
+									RDFS::Resource.new($1)
 								end
 
-			predicate = RDFS::Resource.new(nodes[1])
+      predicate = case nodes[1]
+                  when Resource
+                    RDFS::Resource.new($1)
+                  end
 
 			# handle bnodes and literals if necessary (literals need unicode fixing)
 			object = case nodes[2]
 							 when BNode
 								 RDFS::Resource.new("http://www.activerdf.org/bnode/#{uuid}/#$1")
 							 when Literal
-								 fix_unicode(nodes[2])
-							 else
-								 RDFS::Resourec.new(nodes[2])
+								 fix_unicode($1)
+               when Resource
+								 RDFS::Resource.new($1)
 							 end
 
       # collect s, p, o into array to be returned
@@ -44,7 +54,7 @@ class NTriplesParser
 
 	private
 	# constants for extracting resources/literals from sql results
-	Node = Regexp.union(/_:\S*/,/<[^>]*>/,/"[^"]*"/)
+	Node = Regexp.union(/"[^"]*"/,/_:\S*/,/<[^>]*>/)
 	BNode = /_:(\S*)/
 	Resource = /<([^>]*)>/
 	Literal = /"([^"]*)"/
