170937e62b290cabb5e950cff93b2d21c8e0e3e2
daniel <ghub@limitedcreativity.org>
2007-09-07 09:58:08 +0000

    Preparation for tesly
    git-svn-id: http://svn.talia.discovery-project.eu/talia/ActiveRDF/trunk@96 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- test/common.rb
+++ test/common.rb
@@ -1,3 +1,9 @@
+# Check for the tesly adapter, and load it if it's there
+if(File.exists?(File.dirname(__FILE__) + '/tesly_reporter.rb'))
+  printf("Continuing with tesly \n")
+  require File.dirname(__FILE__) + '/tesly_reporter'
+end
+
 def get_adapter
 	types = ConnectionPool.adapter_types
 	if types.include?(:rdflite)
